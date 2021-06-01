364ccfd08a4e5c9aa431e1a0677ab134c8d193a0
Noah Dörr <archecraft1@gmail.com>
2021-06-01 22:35:52 +0200

    Make Advancements available without the right power

--- src/main/java/com/archecraft/minecraft/acorigins/mixin/PlayerEntityMixin.java
+++ src/main/java/com/archecraft/minecraft/acorigins/mixin/PlayerEntityMixin.java
@@ -1,16 +1,22 @@
 package com.archecraft.minecraft.acorigins.mixin;
 
+import com.archecraft.minecraft.acorigins.components.ACODataComponent;
 import com.archecraft.minecraft.acorigins.powers.WitherKillBonusPower;
+import com.archecraft.minecraft.acorigins.registry.ACOComponents;
 import com.archecraft.minecraft.acorigins.registry.ACOPowers;
 import io.github.apace100.origins.component.OriginComponent;
 import io.github.apace100.origins.power.StatusEffectPower;
 import io.github.apace100.origins.registry.ModComponents;
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementProgress;
 import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.EffectCommand;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import org.graalvm.compiler.virtual.phases.ea.EffectsClosure;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,7 +39,23 @@ public abstract class PlayerEntityMixin {
     @Inject(method = "onKilledOther", at = @At("HEAD"))
     private void witherKilled(ServerWorld serverWorld, LivingEntity livingEntity, CallbackInfo ci) {
         if (livingEntity.getType() == EntityType.WITHER) {
+            ACODataComponent data = ACOComponents.DATA.get(this);
+            
+            data.incrementWithersKilled();
+    
             ModComponents.ORIGIN.get(this).getPowers(WitherKillBonusPower.class).forEach(WitherKillBonusPower::killedWither);
+    
+            if (data.getWithersKilled() >= 100) {
+                if ((Object) this instanceof ServerPlayerEntity) {
+                    Advancement advancement = ((PlayerEntity)(Object) this).getServer().getAdvancementLoader().get(Identifier.tryParse("acorigins:withers_killed"));
+                    AdvancementProgress progress = ((ServerPlayerEntity)(Object) this).getAdvancementTracker().getProgress(advancement);
+                    if (!progress.isDone()) {
+                        for (String criteria : progress.getUnobtainedCriteria()) {
+                            ((ServerPlayerEntity)(Object) this).getAdvancementTracker().grantCriterion(advancement, criteria);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- src/main/java/com/archecraft/minecraft/acorigins/powers/WitherKillBonusPower.java
+++ src/main/java/com/archecraft/minecraft/acorigins/powers/WitherKillBonusPower.java
@@ -16,8 +16,6 @@ public class WitherKillBonusPower extends ACODataPower {
     
     public void killedWither() {
         if (isActive()) {
-            data.incrementWithersKilled();
-            
             double random = player.getRandom().nextDouble();
             if (random < 0.05) {
                 data.setSpeed(data.getSpeed() + 0.5);
@@ -29,18 +27,6 @@ public class WitherKillBonusPower extends ACODataPower {
                 data.setHealth(data.getHealth() + 1);
             } else {
                 data.setDamage(data.getDamage() + 1);
-            }
-            
-            if (data.getWithersKilled() >= 100) {
-                if (player instanceof ServerPlayerEntity) {
-                    Advancement advancement = player.getServer().getAdvancementLoader().get(Identifier.tryParse("acorigins:withers_killed"));
-                    AdvancementProgress progress = ((ServerPlayerEntity) player).getAdvancementTracker().getProgress(advancement);
-                    if (!progress.isDone()) {
-                        for (String criteria : progress.getUnobtainedCriteria()) {
-                            ((ServerPlayerEntity) player).getAdvancementTracker().grantCriterion(advancement, criteria);
-                        }
-                    }
-                }
             }
         }
     }
